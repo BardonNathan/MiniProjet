519d2625be7078de9d04ed5e0aaac8969e6c7f26
BARDON Nathan <bardon.nathan@gmail.com>
2019-12-02 23:50:14 +0100

    Add 7 segments support. Add synchronization

--- MiniProjet.hpp
+++ MiniProjet.hpp
@@ -3,6 +3,7 @@
 
 /******************
  * TODO : 
+ *  - Test if MP_ERR_NO_ETHERNET_SHIELD working fine
  *  - At the end of init(), sync ip addr with server
  *  - When HTTP req failed => retry with 5 times.
  *  -     \ If 5 fails occurs, stop program check why (no shield, Ethernet unplugged..) and inform the user
@@ -26,12 +27,16 @@
 #define MP_ERR_ETHERNET_UNPLUGGED       2
 #define MP_ERR_NO_DHCP                  3
 #define MP_ERR_TIMEOUT                  4
-#define MP_ERR_BUFFER_OVERFLOW          5
+
 #define MP_ERR_CONNECTION_FAILED        6
 #define MP_ERR_PROXY_CONNECTION_FAILED  7
 #define MP_ERR_ALREADY_SYNC             8
 
 #define MP_ERR_UNKNOWN_ERR             -1
+
+#define MP_VARNAME_LED1 "led1"
+#define MP_VARNAME_LED2 "led2"
+#define MP_VARNAME_7SEG "7segs"
 
 namespace mp {
   byte MAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -42,12 +47,32 @@ namespace mp {
   IPAddress subnet(255, 255, 255, 0);
 
   bool proxyNeeded = false;
-  long lastSync = 0;
+  long nextSync = 0;
+
+  struct {
+    bool led1, led2, btn1, btn2, btn3;
+    short lum, temp, pota1, pota2, pota3, segments;
+  } varval;
+  
+  byte segs_template[] {
+    0b10000010, // 0
+    0b10111011, // 1
+    0b10000101, // 2
+    0b10010001, // 3
+    0b10111000, // 4
+    0b11010000, // 5
+    0b11000000, // 6
+    0b10011011, // 7
+    0b10000000, // 8
+    0b10010000  // 9
+  };
 
   // ----------------------------------------------------------------------------------------------------------------------------
 
   /** Init MiniProj
     *  @return 0 on success or error code. See MP_ERR_* preprocessor defines.
+    *  
+    * Potential errors : MP_ERR_NO_ETHERNET_SHIELD, MP_ERR_ETHERNET_UNPLUGGED, MP_ERR_NO_DHCP (warning, not an error).
     */
   int init() {
     if (!Ethernet.begin(MAC)) {
@@ -66,13 +91,27 @@ namespace mp {
     return 0;
   }
 
+  // ----------------------------------------------------------------------------------------------------------------------------
+  /** Function used internaly to change variables values (led state and 7 seg value) after a sync.
+    */
+  void execVariable(String varname, String varval) {
+
+    if (varname == MP_VARNAME_LED1)
+      mp::varval.led1 = (varval == "1");
+    
+    else if (varname == MP_VARNAME_LED2)
+      mp::varval.led2 = (varval == "1");
+
+    else if (varname == MP_VARNAME_7SEG)
+      mp::varval.segments = varval.toInt();
+  }
 
 
   // ----------------------------------------------------------------------------------------------------------------------------
   /** Process an HTTP request to the server in order to sync data and push others.
     * @return 0 on success or error code. See MP_ERR_* preprocessor defines.
     * 
-    * Potential errors : MP_ERR_TIMEOUT, MP_ERR_BUFFER_OVERFLOW, MP_ERR_CONNECTION_FAILED.
+    * Potential errors : MP_ERR_TIMEOUT, MP_ERR_CONNECTION_FAILED, MP_ERR_PROXY_CONNECTION_FAILED.
     */
   int processHTTPRequest() {
     EthernetClient c;
@@ -89,7 +128,19 @@ namespace mp {
     // Send HTTP request
     c.print("GET http://");
     c.print(MP_SRV);
-    c.println("/sync.php HTTP/1.1");
+    c.print("/sync.php?");
+
+    // Send sensor values
+    c.print("btn1=");  c.print(varval.btn1);
+    c.print("&btn2=");  c.print(varval.btn2);
+    c.print("&btn3=");  c.print(varval.btn3);
+    c.print("&pota1="); c.print(varval.pota1);
+    c.print("&pota2="); c.print(varval.pota2);
+    c.print("&pota3="); c.print(varval.pota3);
+    c.print("&temp=");  c.print(varval.temp);
+    c.print("&lum=");   c.print(varval.lum);
+    
+    c.println(" HTTP/1.1");
 
     c.print("Host: ");
     c.println(MP_SRV);
@@ -129,11 +180,11 @@ namespace mp {
           
       } else {
         if (cbuff == ';') {  
+          execVariable(varname, varval);
+          
           varname = "";
           varval  = "";
           isName  = true;
-
-          execVariable(varname, varval);
         } else
           varval += (char)cbuff;
       }
@@ -147,6 +198,8 @@ namespace mp {
   // ----------------------------------------------------------------------------------------------------------------------------
   /** Synchronize local sensors with server and server's data with actuators.
     *  @return 0 on success or error code. See MP_ERR_* preprocessor defines.
+    *  
+    * Potential errors : MP_ERR_ALREADY_SYNC and processHTTPRequest()'s errors.
     */
   int sync() {
     if (nextSync > millis())
@@ -154,6 +207,48 @@ namespace mp {
 
     nextSync = millis() + MP_SYNC_DELAY;
     
-    processHTTPRequest();
+    return processHTTPRequest();
+  }
+
+
+
+  
+  // ----------------------------------------------------------------------------------------------------------------------------
+  /** Update 7 segments display from mp::varval.segments.
+    */
+  void update7seg(int act1Pin, int act2Pin, int sigPin, int clkPin, int trigPin) {
+    bool first = true;
+    
+    do {
+      digitalWrite(act1Pin,  first);
+      digitalWrite(act2Pin, !first);
+      
+      //                 get tens                       get units
+      byte n = first ? (mp::varval.segments / 10) % 100 : mp::varval.segments % 10;
+      n = (n < 0 || n > 9) ? 0b01111111 : segs_template[n];
+      Serial.print(first ?"a":"b");
+      Serial.println(n, BIN);
+    
+      // For each bits, write HIGH level for a 1 or LOW for a 0 to register's SIG pin
+      //                Pulse clock
+      for (int i = 0 ; i < 8 ; i++) {
+        digitalWrite(sigPin, (n<<i) & 0x80);
+        digitalWrite(clkPin, HIGH);
+        delayMicroseconds(10);
+        
+        digitalWrite(sigPin, LOW);
+        digitalWrite(clkPin, LOW);
+        delayMicroseconds(10);
+      }
+    
+      // Trigger data to output
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(10);
+      first = !first;
+    } while (!first);
+
+    digitalWrite(act2Pin, LOW);
   }
 }
--- MiniProjet.ino
+++ MiniProjet.ino
@@ -1,10 +1,28 @@
 #include "MiniProjet.hpp"
 
+#define PIN_LED1  5
+#define PIN_LED2  6
+#define PIN_BTN1  0
+#define PIN_BTN2  0
+#define PIN_BTN3  0
+
+#define PIN_POTA3 A1
+#define PIN_POTA2 A0
+#define PIN_POTA1 A2
+#define PIN_LUM   A3
+#define PIN_TEMP  A4
+
+#define PIN_7SEGA 9
+#define PIN_7SEGB 2
+#define PIN_REG_SIN 3
+#define PIN_REG_CLK 8
+#define PIN_REG_TRG 7
+
 void setup() {
   Serial.begin(9600);
-  
+
   Serial.println("Initializing Ethernet using DHCP...");
-  // Init Mini Projet (ethernet shiled and variables)
+
   switch (mp::init()) {
     case MP_ERR_NO_ETHERNET_SHIELD:
       Serial.println("Ethernet shield was not found.");
@@ -25,14 +43,14 @@ void setup() {
   Serial.print("Gateway : ");    Serial.println(Ethernet.gatewayIP());
   Serial.print("DNS Server : "); Serial.println(Ethernet.dnsServerIP());
 
-  long aze = micros();
-  int i = mp::processHTTPRequest();
-  aze = (micros() - aze) / 1000;
-  
-  Serial.print("\n== Done in ");
-  Serial.print(aze);
-  Serial.print("ms with error code ");
-  Serial.println(i);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_REG_SIN, OUTPUT);
+  pinMode(PIN_REG_CLK, OUTPUT);
+  pinMode(PIN_REG_TRG, OUTPUT);
+  pinMode(PIN_BTN1, INPUT_PULLUP);
+  pinMode(PIN_BTN2, INPUT_PULLUP);
+  pinMode(PIN_BTN3, INPUT_PULLUP);
 }
 
 
@@ -40,6 +58,35 @@ void setup() {
   
 
 void loop() {
-  mp::sync();
+  long uptimems = millis();
 
+  int sync_result = mp::sync();
+  if (sync_result == 0) {
+    digitalWrite(PIN_LED1, mp::varval.led1);
+    digitalWrite(PIN_LED2, mp::varval.led2);
+  
+  
+  }
+  
+  if (sync_result != MP_ERR_ALREADY_SYNC) {
+    Serial.print("Synchronized in ");
+    Serial.print(millis() - uptimems);
+    Serial.print("ms with error code ");
+    Serial.println(sync_result);
+  }
+  
+  // Update sensor values
+  mp::varval.temp  = (analogRead(PIN_TEMP) *5 * 110)+ 50 / 1000;
+  mp::varval.lum   = analogRead(PIN_LUM) / 10;
+  
+  mp::varval.pota1 = analogRead(PIN_POTA1) / 10; // 0-1024 => 0-100
+  mp::varval.pota2 = analogRead(PIN_POTA2) / 10;
+  mp::varval.pota3 = analogRead(PIN_POTA3) / 10;
+  
+  mp::varval.btn1  = digitalRead(PIN_BTN1);
+  mp::varval.btn2  = digitalRead(PIN_BTN2);
+  mp::varval.btn3  = digitalRead(PIN_BTN3);
+
+  
+  mp::update7seg(PIN_7SEGA, PIN_7SEGB, PIN_REG_SIN, PIN_REG_CLK, PIN_REG_TRG);
 }
